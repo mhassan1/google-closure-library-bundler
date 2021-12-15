35455b7f2b27995b1bd7bb3aa4ed3434b135bafd
mhassan1 <marc.j.hassan@gmail.com>
2021-12-15 10:16:45 -0500

    Fix travis

--- test/bundle.test.js
+++ test/bundle.test.js
@@ -6,7 +6,8 @@ describe('bundle', () => {
   it('creates a bundle', async () => {
     await bundle('./test/fixtures/index.js', {
       path: join(__dirname, 'fixtures'),
-      filename: 'output.js'
+      filename: 'output.js',
+      hashFunction: 'md5'
     })
     expect(readFileSync('./test/fixtures/output.js', 'utf8')).toEqual(readFileSync('./test/fixtures/expected.js', 'utf8'))
   })
@@ -14,7 +15,8 @@ describe('bundle', () => {
   it('fails to create a bundle with invalid inputs', async () => {
     expect(bundle('./test/fixtures/i-do-not-exist.js', {
       path: join(__dirname, 'fixtures'),
-      filename: 'output.js'
+      filename: 'output.js',
+      hashFunction: 'md5'
     })).rejects.toThrow()
   })
 })
