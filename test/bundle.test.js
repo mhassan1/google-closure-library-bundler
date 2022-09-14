3a338d4172336173ac3713204b556dd286447ceb
mhassan1 <marc.j.hassan@gmail.com>
2022-09-13 21:27:24 -0400

    Fix test on windows

--- test/bundle.test.js
+++ test/bundle.test.js
@@ -2,6 +2,8 @@ const { bundle } = require('..')
 const { join } = require('path')
 const { readFileSync } = require('fs')
 
+const normalizeLineEndings = str => str.replace(/\r?\n/g, '\n');
+
 describe('bundle', () => {
   it('creates a bundle', async () => {
     await bundle('./test/fixtures/index.js', {
@@ -9,9 +11,9 @@ describe('bundle', () => {
       filename: 'output.js',
       hashFunction: 'md5'
     })
-    expect(readFileSync('./test/fixtures/output.js', 'utf8')).toEqual(readFileSync('./test/fixtures/expected.js', 'utf8'))
+    expect(readFileSync('./test/fixtures/output.js', 'utf8')).toEqual(normalizeLineEndings(readFileSync('./test/fixtures/expected.js', 'utf8')))
     expect(() => require('./fixtures/output').string.Const.from('hello')).not.toThrow()
-  })
+  }, 15000)
 
   it('fails to create a bundle with invalid inputs', async () => {
     expect(bundle('./test/fixtures/i-do-not-exist.js', {
